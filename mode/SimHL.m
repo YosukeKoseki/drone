8c7b3f8e54a2376d430ccd73eab622e86095dc99
1912063 <g2212044@tcu.ac.jp>
2025-04-18 12:15:35 +0900

    プログラム解読
    ・シミュレーション時間変更
    ・円軌道へ変更
    ・SimHLを回してみていろいろチェック
    →ノミナルモデルは重り分大きくなってる．(0.58kg)

--- mode/SimHL.m
+++ mode/SimHL.m
@@ -1,6 +1,6 @@
 ts = 0; % initial time
 dt = 0.025; % sampling period
-te = 30; % terminal time
+te = 15; % terminal time
 time = TIME(ts,dt,te); % instance of time class
 in_prog_func = @(app) dfunc(app); % in progress plot
 post_func = @(app) dfunc(app); % function working at the "draw button" pushed.
@@ -12,12 +12,14 @@ initial_state.v = [0; 0; 0];
 initial_state.w = [0; 0; 0];
 
 agent = DRONE;
-agent.parameter = DRONE_PARAM("DIATONE","row","mass",0.58);
+% agent.parameter = DRONE_PARAM("DIATONE","row","mass",0.58); %おそらくノミナルモデルの質量→重りをつけている想定担っている　2025.04.17小関
 agent.plant = MODEL_CLASS(agent,Model_Quat13(dt, initial_state, 1));
 agent.parameter.set("mass",struct("mass",0.5))
 agent.estimator = EKF(agent, Estimator_EKF(agent,dt,MODEL_CLASS(agent,Model_EulerAngle(dt, initial_state, 1)),["p", "q"]));
 agent.sensor = MOTIVE(agent, Sensor_Motive(1,0, motive));
-agent.reference = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",5,"orig",[0;0;1],"size",[2,2,0.5]},"HL"});
+
+agent.reference = TIME_VARYING_REFERENCE(agent,{"gen_ref_circle",{"freq",5,"init",[0;0;1],"radius",1},"HL"});
+% agent.reference = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",5,"orig",[0;0;1],"size",[2,2,0.5]},"HL"});
 % agent.reference = TIME_VARYING_REFERENCE(agent,{"Momose_Case_study_trajectory",{[0,0,0]},"HL"});
 % agent.reference = MY_WAY_POINT_REFERENCE(agent,generate_spline_curve_ref(readmatrix("waypoint.xlsx",'Sheet','MyNewSheet'),1));%コマンドでシートを選びたいときは位置2を1にする
 
